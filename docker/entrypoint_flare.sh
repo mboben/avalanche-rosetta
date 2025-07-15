3f5e89a7beea62a3f1b9bd9390796d24f55c1aba
Marko Boben <marko.boben@gmail.com>
2025-07-15 13:06:19 +0200

    Merge Flare changes

---
+++ docker/entrypoint_flare.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -eo pipefail
+if [ "$DEBUG" = "true" ]; then
+	set -o xtrace
+	export LOG_LEVEL=${LOG_LEVEL:-debug}
+fi
+sleep 1
+
+
+export NETWORK_ID=${NETWORK_ID:?'Env var NETWORK_ID is required! Exiting...'}
+export CHAIN_CONFIG_DIR=${CHAIN_CONFIG_DIR:-/app/conf/${NETWORK_ID}}
+export LOG_LEVEL=${LOG_LEVEL:-warn}
+
+if [ "$NETWORK_ID" != "flare" ] && [ "$NETWORK_ID" != "costwo" ] && [ "$NETWORK_ID" != "localflare" ]; then
+    echo "NETWORK_ID value '${NETWORK_ID}' is not a valid network ID! Exiting..."
+    exit 1
+fi
+
+if [ "$NETWORK_ID" = "flare" ]; then
+	export AUTOCONFIGURE_BOOTSTRAP_ENDPOINT=${AUTOCONFIGURE_BOOTSTRAP_ENDPOINT:-'https://flare-bootstrap.flare.network/ext/info'}
+elif [ "$NETWORK_ID" = "costwo" ]; then
+	export AUTOCONFIGURE_BOOTSTRAP_ENDPOINT=${AUTOCONFIGURE_BOOTSTRAP_ENDPOINT:-'https://coston2-bootstrap.flare.network/ext/info'}
+fi
+
+if [ "$FLARE_LOCAL_TXS_ENABLED" = "true" ]; then
+	jq --argjson var "true" '."local-txs-enabled"=$var' "${CHAIN_CONFIG_DIR}/C/config.json" | sponge "${CHAIN_CONFIG_DIR}/C/config.json"
+fi
+
+if [ ! -z "$AUTOCONFIGURE_BOOTSTRAP_ENDPOINT" ];
+then
+	echo "Autoconfiguring bootstrap IPs and IDs with endpoint '${AUTOCONFIGURE_BOOTSTRAP_ENDPOINT}'"
+
+	# Check if we can connect to the bootstrap endpoint (whitelisting)
+	BOOTSTRAP_STATUS=$(curl -m 10 -s -w %{http_code} -X POST  --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' "$AUTOCONFIGURE_BOOTSTRAP_ENDPOINT" -o /dev/null)
+	if [ "$BOOTSTRAP_STATUS" != "200" ]; then
+		echo "Could not connect to bootstrap endpoint. Is your IP whitelisted?"
+		exit 1
+	fi
+
+	if [ ! -z "$BOOTSTRAP_IPS" ]; then echo "BOOTSTRAP_IPS is defined ('${BOOTSTRAP_IPS}'), skipping autoconfigure";
+	else BOOTSTRAP_IPS=$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' "$AUTOCONFIGURE_BOOTSTRAP_ENDPOINT" | jq -r ".result.ip")
+	fi
+
+	if [ ! -z "$BOOTSTRAP_IDS" ]; then echo "BOOTSTRAP_IDS is defined ('${BOOTSTRAP_IDS}'), skipping autoconfigure";
+	else BOOTSTRAP_IDS=$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' "$AUTOCONFIGURE_BOOTSTRAP_ENDPOINT" | jq -r ".result.nodeID")
+	fi
+fi
+
+/app/flare/build/avalanchego \
+	--http-host="$HTTP_HOST" \
+	--http-port="$HTTP_PORT" \
+	--staking-port="$STAKING_PORT" \
+	--public-ip="$PUBLIC_IP" \
+	--db-dir="$DB_DIR" \
+	--db-type="$DB_TYPE" \
+	--bootstrap-ips="$BOOTSTRAP_IPS" \
+	--bootstrap-ids="$BOOTSTRAP_IDS" \
+	--chain-config-dir="$CHAIN_CONFIG_DIR" \
+	--log-dir="$LOG_DIR" \
+	--log-level="$LOG_LEVEL" \
+	--network-id="$NETWORK_ID" \
+	--api-keystore-enabled=true \
+	--http-allowed-hosts="$HTTP_ALLOWED_HOSTS" \
+	$EXTRA_ARGUMENTS
---
+++ docker/entrypoint_rosetta.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+set -eo pipefail
+if [ "$DEBUG" = "true" ]; then set -o xtrace; fi
+sleep 1 # REQUIRED! So the entrypoint_main.sh registers the wait -n before this one fails.
+
+export NETWORK_ID=${NETWORK_ID:?'Env var NETWORK_ID is required! Exiting...'}
+export ROSETTA_FLARE_ENDPOINT=${ROSETTA_FLARE_ENDPOINT:?'Env var ROSETTA_FLARE_ENDPOINT is required! Exiting...'}
+export ROSETTA_CONFIG_PATH=${ROSETTA_CONFIG_PATH:-/app/conf/$NETWORK_ID/server-config.json}
+
+
+if [ "$START_ROSETTA_SERVER_AFTER_BOOTSTRAP" != "true" ] && [ "$START_ROSETTA_SERVER_AFTER_BOOTSTRAP" != "false" ]; then
+    echo "No valid argument was passed for START_ROSETTA_SERVER_AFTER_BOOTSTRAP, using default: false"
+    START_ROSETTA_SERVER_AFTER_BOOTSTRAP=false
+fi
+
+if [ "$MODE" = "online" ]; then
+
+    # Wait for go-flare port to bind
+    sleep 30
+    curl ${ROSETTA_FLARE_ENDPOINT} --retry 6 --retry-connrefused --retry-all-errors --connect-timeout 5 --retry-delay 10 --silent --output /dev/null
+
+    if [ "$START_ROSETTA_SERVER_AFTER_BOOTSTRAP" == "true" ]; then
+        while true; do
+            result=$(curl -X POST --data '{"jsonrpc": "2.0","method": "info.isBootstrapped","params":{"chain":"C"},"id":1}' -H 'Content-Type: application/json' -H 'cache-control: no-cache' -s ${ROSETTA_FLARE_ENDPOINT}/ext/info)
+            is_bootstrapped=$(echo $result | jq '.result.isBootstrapped')
+            if [ "$is_bootstrapped" = "true" ]; then
+                echo "[rosetta-start-script] Network '$NETWORK_ID' is bootstrapped, OK!"
+                break
+            fi
+            echo "[rosetta-start-script] Network '$NETWORK_ID' NOT bootstrapped, retrying..."
+            sleep 1
+        done
+    fi
+
+
+    while true
+    do
+        STATUS=$(curl -m 10 -s -w %{http_code} ${ROSETTA_FLARE_ENDPOINT}/ext/health -o /dev/null)
+        if [ $STATUS = "200" ]; then
+            echo "[rosetta-start-script] Got status '$STATUS' on network id '$NETWORK_ID', OK!"
+            break
+        elif [ "$START_ROSETTA_SERVER_AFTER_BOOTSTRAP" = "false" ] && [ $STATUS = "503" ]; then
+            echo "[rosetta-start-script] Got status '$STATUS' but we are not waiting for flare to fully bootstrap, OK!"
+            break     
+        elif [ $STATUS = "503" ] && [ $NETWORK_ID = "localflare" ]; then
+            echo "[rosetta-start-script] Got status '$STATUS' on network id '$NETWORK_ID', checking if because of no peers"
+            is_because_of_no_peers=$(curl -s ${ROSETTA_FLARE_ENDPOINT}/ext/health | grep "network layer is unhealthy reason: not connected to a minimum of 1 peer")
+
+            if [ ! -z is_because_of_no_peers ]; then
+                echo "[rosetta-start-script] it is because there are no peers. This is okay on localflare. OK!"
+                break
+            fi
+        fi
+        echo "[rosetta-start-script] Node RPC not ready yet, got response status $STATUS on network id '$NETWORK_ID', retrying..."
+        sleep 1
+    done
+
+    jq --arg c "${ROSETTA_FLARE_ENDPOINT}" '.rpc_endpoint=$c' "${ROSETTA_CONFIG_PATH}" | sponge "${ROSETTA_CONFIG_PATH}"
+
+fi
+
+
+jq --arg m "${MODE}" '.mode=$m' "${ROSETTA_CONFIG_PATH}" | sponge "${ROSETTA_CONFIG_PATH}"
+
+
+/app/rosetta-server/rosetta-server -config=${ROSETTA_CONFIG_PATH}
